5e4ee11d6de11ed0716f6d69fd9e6ffc21c5e5b3
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-02-23 20:59:05 +0900

    Update server.js
    [test] DB 테스트용 임시코드 추가 (2024.2.23 김도연)

--- server/server.js
+++ server/server.js
@@ -9,3 +9,34 @@ const port = 3002; //node 서버가 사용할 포트 번호, 리액트의 포트
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
+
+/*
+기본 getMapping 문법
+
+app.get('/home', function(요청, 응답){
+    응답.send('내용');
+});
+*/
+
+app.get('/', function (req, res) {
+    res.send('홈');
+    //res.sendFile(__dirname + 'main.html');
+
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'friend_test'
+    });
+
+    connection.connect();
+
+    connection.query('SELECT * from quiz_list', (error, rows, fields) => {
+        if (error) throw error;
+        console.log('quiz_list info is: ', rows);
+    });
+
+    connection.end();
+
+});
