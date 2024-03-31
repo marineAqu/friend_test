1f903c544de12693d4fa39a17f2d0a58419d4056
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-03-31 11:23:25 +0900

    [chore] 테스트를 위해 여러장 업로드로 수정 (2024.3.31 김도연)

--- client/src/pages/make/TempTestPage.jsx
+++ client/src/pages/make/TempTestPage.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 
 const TempTestPage = () => {
-    const [image, setImage] = useState(null);
+    const [images, setImages] = useState([]);
 
     // 이미지 변경 핸들러
     const handleImageChange = (event) => {
-        const selectedImage = event.target.files[0];
-        setImage(selectedImage);
+        const selectedImages = event.target.files;
+        setImages([...images, ...selectedImages]);
     };
 
     // 이미지 업로드 핸들러
     const handleImageUpload = async () => {
-        // 이미지 업로드 로직을 구현합니다. 여기서는 간단히 이미지를 콘솔에 로그합니다.
-        if (image) {
-            console.log('업로드된 이미지:', image);
+        // 이미지 업로드 로직을 구현합니다.
+        if (images.length > 0) {
+            console.log('업로드된 이미지:', images);
 
             const formData = new FormData();
-            formData.append('image', image);
+            images.forEach((image, index) => {
+                console.log("image에 값이 들었는지 확인용: "+JSON.stringify(image));
+                formData.append(`image${index + 1}`, image);
+            });
 
             try {
                 const response = await fetch('/tempTest', {
@@ -36,14 +39,17 @@ const TempTestPage = () => {
     return (
         <div>
             <h2>이미지 업로더</h2>
+            {/* 여러 개의 input 요소를 추가합니다. */}
             <input type="file" id={'tempImg'} accept="image/*" onChange={handleImageChange} />
+            <input type="file" id={'tempImg2'} accept="image/*" onChange={handleImageChange} />
+            {/* 필요에 따라 더 많은 input 요소를 추가할 수 있습니다. */}
             <button onClick={handleImageUpload}>이미지 업로드</button>
-            {image && (
-                <div>
-                    <h3>선택된 이미지</h3>
-                    <img src={URL.createObjectURL(image)} alt="선택된 이미지" width="200" />
+            {images.map((image, index) => (
+                <div key={index}>
+                    <h3>{`선택된 이미지 ${index + 1}`}</h3>
+                    <img src={URL.createObjectURL(image)} alt={`선택된 이미지 ${index + 1}`} width="200" />
                 </div>
-            )}
+            ))}
         </div>
     );
 };
